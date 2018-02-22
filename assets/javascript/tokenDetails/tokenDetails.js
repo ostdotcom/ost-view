8d4d7e4d5e0935129bd4168cd61b7f4363d4963e
Akshay <akshay.raje@gmail.com>
2018-02-22 21:00:26 +0530

    Datatable styling + some other stuff

--- assets/javascript/tokenDetails/tokenDetails.js
+++ assets/javascript/tokenDetails/tokenDetails.js
@@ -5,12 +5,6 @@
 $(document).ready(function() {
   var transaction_url = document.getElementById("transactionUrl").innerHTML;
 
-  $("#test").on("click", function () {
-    console.log("test clicked! click");
-  });
-
-  var something;
-
   var dtConfig = {
     "bLengthChange": false,
     "searching": false,
@@ -30,65 +24,70 @@ $(document).ready(function() {
     return Handlebars.compile(string.replace(/\[\[/g, '{{').replace(/\]\]/g, '}}'));
   };
 
-  var dt_col_1 = handlebarsCompile($('#dt-col-1').text());
-  var dt_col_2 = handlebarsCompile($('#dt-col-2').text());
-  var dt_col_3 = handlebarsCompile($('#dt-col-3').text());
-  var dt_col_4 = handlebarsCompile($('#dt-col-4').text());
-  var dt_col_5 = handlebarsCompile($('#dt-col-5').text());
-  var dt_col_6 = handlebarsCompile($('#dt-col-6').text());
-  var dt_col_7 = handlebarsCompile($('#dt-col-7').text());
-
+  var dt_cols = [
+    handlebarsCompile($('#dt-col-1').text()),
+    handlebarsCompile($('#dt-col-2').text()),
+    handlebarsCompile($('#dt-col-3').text()),
+    handlebarsCompile($('#dt-col-4').text()),
+    handlebarsCompile($('#dt-col-5').text()),
+    handlebarsCompile($('#dt-col-6').text()),
+    handlebarsCompile($('#dt-col-7').text())
+  ];
    dtConfig.columns.unshift(
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return dt_col_1({
+        return dt_cols[0]({
           symbol: 'AKC',
           name: 'Akshay Coin'
         });
       }
     },
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return '<div class="text-truncate d-inline-block tokenDetailsMaxWidth"><img src="https://dummyimage.com/400x400/22aaee/fff.png" class="tokenIcon" /> <span class="">'+ data.tokens +'</span></div>';
+        return dt_cols[1]({
+          tokens: data.tokens,
+          value: 100
+        });
       }
     },
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return '<div class="text-truncate d-inline-block tableBorderRight"> <img src="https://dummyimage.com/400x400/22aaee/fff.png" class="tokenIcon" /><span class="">'+ moment(data.timestamp * 1000).startOf('day').fromNow()  +'</span> </div>';
+        return dt_cols[2]({
+          timestamp: moment(data.timestamp * 1000).startOf('day').fromNow()
+        });
       }
     },
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return '<div class=" text-truncate d-inline-block tokenDetailsMaxWidth"><span class="tokenDetailsColora84"> TX#</span><span class="default_bright_blue">'+data.transaction_hash +'</span></div>';
+        return dt_cols[3]({
+          tx: data.transaction_hash
+        });
       }
     },
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return '<div class=" text-truncate d-inline-block tokenDetailsMaxWidth"><span class="tokenDetailsColora84"> From </span><span class="default_bright_blue">'+ data.t_from+' </span></div>';
+        return dt_cols[4]({
+          from: data.t_from
+        });
       }
     },
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return '<img src="https://dummyimage.com/400x400/22aaee/fff.png" class="tokenIcon" />';
+        return dt_cols[5]();
       }
     },
     {
-      title: '',
       data: null,
       render: function(data, type, full, meta){
-        return '<div class=" text-truncate d-inline-block tokenDetailsMaxWidth"><span class="tokenDetailsColora84"> To </span><span class="default_bright_blue">'+ data.t_to +' </span></div>';
+        return dt_cols[6]({
+          to: data.t_to
+        });
       }
     }
   );
--- helpers/handlebar_helper.js
+++ helpers/handlebar_helper.js
@@ -38,4 +38,18 @@ module.exports = {
     }[operator];
   },
 
+  block: function (name) {
+    var blocks  = this._blocks,
+      content = blocks && blocks[name];
+
+    return content ? content.join('\n') : null;
+  },
+
+  contentFor: function (name, options) {
+    var blocks = this._blocks || (this._blocks = {}),
+      block  = blocks[name] || (blocks[name] = []);
+
+    block.push(options.fn(this));
+  }
+
 }
