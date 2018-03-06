11f8f349efb417e347549fdce98ef0a95f723646
pankajkeswani <pankaj@pepo.com>
2018-03-06 21:17:33 +0530

    Unhandled some exception case

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -6,7 +6,7 @@
 
 # chain env 2000
 export OST_VIEW_0_CHAIN_ID="2000"
-export OST_VIEW_0_WEB_RPC="http://127.0.0.1:9546"
+export OST_VIEW_0_WEB_RPC="http://devUtilityChain.com:9546"
 
 #DB env
 export OST_VIEW_0_DB_USER="root"
@@ -19,6 +19,34 @@ export OST_VIEW_0_DB_HOST="localhost"
 export OST_VIEW_0_DB_CONNECTION_LIMIT=10
 
 
+# chain env 2001
+export OST_VIEW_1_CHAIN_ID="2001"
+export OST_VIEW_1_WEB_RPC="http://devValueChain.com:8545"
+
+#DB env
+export OST_VIEW_1_DB_USER="root"
+export OST_VIEW_1_DB_PWD="root"
+
+export OST_VIEW_1_DB_NAME="ost_explorer_2001"
+
+export OST_VIEW_1_DB_HOST="localhost"
+
+export OST_VIEW_1_DB_CONNECTION_LIMIT=10
+
+
+# chain env 142
+export OST_VIEW_2_CHAIN_ID="142"
+export OST_VIEW_2_WEB_RPC="http://devUtilityChain.com:9546"
+
+#DB env
+export OST_VIEW_2_DB_USER="root"
+export OST_VIEW_2_DB_PWD="root"
+
+export OST_VIEW_2_DB_NAME="ost_explorer_142"
+
+export OST_VIEW_2_DB_HOST="localhost"
+
+export OST_VIEW_2_DB_CONNECTION_LIMIT=10
 
 # Cache
 export OST_CACHING_ENGINE='memcached'
@@ -30,7 +58,7 @@ export OST_REDIS_PASS=''
 export OST_REDIS_TLS_ENABLED='0'
 
 # Notification
-export OST_RMQ_SUPPORT='1'
+export OST_RMQ_SUPPORT='0'
 export OST_RMQ_HOST='127.0.0.1'
 export OST_RMQ_PORT='5672'
 export OST_RMQ_USERNAME='guest'
@@ -45,7 +73,7 @@ export OST_VIEW_PATH='.'
 
 #Web env variables
 
-export CHAIN_ID='2000';
+export CHAIN_ID='142';
 
 export BASE_CONTRACT_ADDRESS='0';
 
