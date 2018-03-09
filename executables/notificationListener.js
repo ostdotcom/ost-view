19d19461bb2fe9e8fbc5a35bbed40085b0dcbd0b
Alpesh <alpesh@pepo.com>
2018-03-09 17:20:15 +0530

    event subscriber for bt updates.

--- executables/notificationListener.js
+++ executables/notificationListener.js
@@ -13,20 +13,28 @@
 const openSTNotification = require('@openstfoundation/openst-notification');
 
 const rootPrefix = ".."
-    , logger = require(rootPrefix + '/helpers/custom_console_logger')
-    , notificationProcessor = require(rootPrefix + '/lib/notificationProcessor')
-    ;
+  , logger = require(rootPrefix + '/helpers/custom_console_logger')
+  , notificationProcessor = require(rootPrefix + '/lib/notificationProcessor')
+;
 
+var unAckCount = 0;
 
-function subscribe(){
-    openSTNotification.subscribeEvent.rabbit(
-        ['transfer.#'],
-        {queue: 'OpenST-Explorer-Notification-Listener'},
-        function(msgContent){
-            logger.info('[RECEIVED]', msgContent, '\n');
-            processNotification(JSON.parse(msgContent)['message']);
-        }
-    ).catch(function (err) {logger.error(err);});
+function subscribe() {
+  openSTNotification.subscribeEvent.rabbit(
+    ['transfer.#', 'entity.#'],
+    {
+      queue: 'OpenST-Explorer-Notification-Listener',
+      ackRequired: 1,
+      prefetch: 1
+    },
+    function (msgContent) {
+      logger.info('[RECEIVED]', msgContent, '\n');
+      unAckCount++;
+      return processNotification(JSON.parse(msgContent)['message']);
+    }
+  ).catch(function (err) {
+    logger.error(err);
+  });
 }
 
 // Start
@@ -38,9 +46,42 @@ subscribe();
  * @param msgContent
  */
 var processNotification = function (msgContent) {
-    console.log("processNotification" ,msgContent);
-    if (msgContent['kind'] =='transaction_mined') {
-        logger.info("New Transaction mined", msgContent['payload']);
-        notificationProcessor.processTransaction(msgContent.payload);
+  console.log("processNotification", msgContent);
+  return new Promise(function (onResolve, onReject) {
+    if (msgContent['kind'] == 'transaction_mined') {
+      logger.info("New Transaction mined", msgContent['payload']);
+      notificationProcessor.processTransaction(msgContent.payload).then(function () {
+        logger.log("NotificationProcessor#processTransaction :: Transaction type inserted successfully");
+        unAckCount--;
+        onResolve();
+      }).catch(function (err) {
+        logger.error("NotificationProcessor#processTransaction :: Transaction type insertion error", err);
+        unAckCount--;
+        onResolve();
+      });
+    } else if (msgContent['kind'] == 'shared_entity') {
+
+      unAckCount--;
+      //TODO: function call to update branded token info.
+      onResolve();
+
+    } else {
+      unAckCount--;
+      onResolve();
     }
+  });
 };
+
+process.on('SIGINT', function () {
+  console.log('Received SIGINT, checking unAckCount.');
+  var f = function () {
+    if (unAckCount === 0) {
+      process.exit(1);
+    } else {
+      console.log('waiting for open tasks to be done.');
+      setTimeout(f, 1000);
+    }
+  };
+
+  setTimeout(f, 1000);
+});
