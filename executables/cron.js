dd7d902076fb796f7e8b2fc27ecf8bc703850689
Sachin <sachin@pepo.com>
2018-03-21 11:55:44 +0530

    removed cron.js call

--- executables/cron.js
+++ executables/cron.js
@@ -2,11 +2,11 @@
 "use strict";
 
 var CronJob = require('cron').CronJob
-    , rootPrefix = ".."
-    , cliHandler = require('commander')
-    , version = require(rootPrefix + '/package.json').version
-    , logger = require(rootPrefix + "/helpers/custom_console_logger")
-;
+  , rootPrefix = ".."
+  , cliHandler = require('commander')
+  , version = require(rootPrefix + '/package.json').version
+  , logger = require(rootPrefix + "/helpers/custom_console_logger")
+  ;
 var fs = require('fs');
 
 const { spawn } = require('child_process');
@@ -27,31 +27,30 @@ if (!cliHandler.chainID) {
   logger.error('\n\tPlease Specify chain ID \n\t$>node cron.js -c <chainID> -j <jobID>\n');
   process.exit(1);
 } else {
-    chainId = cliHandler.chainID;
-    console.log("Passed Chain Id", chainId);
+  chainId = cliHandler.chainID;
+  console.log("Passed Chain Id", chainId);
 }
 
- var job1 = new CronJob('*/1 * * * *', function() {
-     startBlockFetcher()
- }, null, false, 'Asia/Kolkata');
+var job1 = new CronJob('*/1 * * * *', function () {
+  startBlockFetcher()
+}, null, false, 'Asia/Kolkata');
 
- var job2 = new CronJob('*/1 * * * *', function() {
-     startBlockVerifier()
- }, null, false, 'Asia/Kolkata');
+var job2 = new CronJob('*/1 * * * *', function () {
+  startBlockVerifier()
+}, null, false, 'Asia/Kolkata');
 
- var job3 = new CronJob('*/1 * * * *', function() {
-     startAggregator()
- }, null, false, 'Asia/Kolkata');
+var job3 = new CronJob('*/1 * * * *', function () {
+  startAggregator()
+}, null, false, 'Asia/Kolkata');
 
 
-// startBlockFetcher();
-// startBlockVerifier();
-// startAggregator();
-//
-// job1.start();
-// job2.start();
-// job3.start();
-//
+//startBlockFetcher();
+//startBlockVerifier();
+//startAggregator();
+
+//job1.start();
+//job2.start();
+//job3.start();
 
 // var jobID = String( cliHandler.jobID ).toLowerCase()
 //     , job
@@ -83,68 +82,67 @@ if (!cliHandler.chainID) {
 // job.start();
 
 
-
 function startBlockFetcher() {
 
-    console.log("BlockFetcher initiated");
+  console.log("BlockFetcher initiated");
 
-    const blockFetcher = spawn('./executables/block_fetcher_cron.js', ['-c', chainId]);
+  const blockFetcher = spawn('./executables/block_fetcher_cron.js', ['-c', chainId]);
 
-    blockFetcher.stdout.on('data', function(data) {
-        writeLogsToFile("blockFetcher_process_logs.txt", data);
-    });
+  blockFetcher.stdout.on('data', function (data) {
+    writeLogsToFile("blockFetcher_process_logs.txt", data);
+  });
 
-    blockFetcher.stderr.on('data', function(data) {
-        writeLogsToFile("blockFetcher_error_logs.txt", data);
-    });
+  blockFetcher.stderr.on('data', function (data) {
+    writeLogsToFile("blockFetcher_error_logs.txt", data);
+  });
 
-    blockFetcher.on('close', function(code) {
-        console.log("blockFetcher child process exited with code", code);
-    });
+  blockFetcher.on('close', function (code) {
+    console.log("blockFetcher child process exited with code", code);
+  });
 }
 
 function startBlockVerifier() {
 
-    console.log("BlockVerifier initiated");
+  console.log("BlockVerifier initiated");
 
-    const blockFetcher = spawn('./executables/block_verifier_cron.js', ['-c', chainId]);
+  const blockFetcher = spawn('./executables/block_verifier_cron.js', ['-c', chainId]);
 
-    blockFetcher.stdout.on('data', function(data) {
-        writeLogsToFile("blockVerifier_process_logs.txt", data);
-    });
+  blockFetcher.stdout.on('data', function (data) {
+    writeLogsToFile("blockVerifier_process_logs.txt", data);
+  });
 
-    blockFetcher.stderr.on('data', function(data) {
-        writeLogsToFile("blockVerifier_error_logs.txt", data);
-    });
+  blockFetcher.stderr.on('data', function (data) {
+    writeLogsToFile("blockVerifier_error_logs.txt", data);
+  });
 
-    blockFetcher.on('close', function(code) {
-        console.log("blockVerifier child process exited with code", code);
-    });
+  blockFetcher.on('close', function (code) {
+    console.log("blockVerifier child process exited with code", code);
+  });
 }
 
 function startAggregator() {
 
-    console.log("Aggregator initiated");
+  console.log("Aggregator initiated");
 
-    const blockFetcher = spawn('./executables/aggregator_cron.js', ['-c', chainId]);
+  const blockFetcher = spawn('./executables/aggregator_cron.js', ['-c', chainId]);
 
-    blockFetcher.stdout.on('data', function(data) {
-        writeLogsToFile("aggregator_process_logs.txt", data);
-    });
+  blockFetcher.stdout.on('data', function (data) {
+    writeLogsToFile("aggregator_process_logs.txt", data);
+  });
 
-    blockFetcher.stderr.on('data', function(data) {
-        writeLogsToFile("aggregatorCron_error_logs.txt", data);
-    });
+  blockFetcher.stderr.on('data', function (data) {
+    writeLogsToFile("aggregatorCron_error_logs.txt", data);
+  });
 
-    blockFetcher.on('close', function(code) {
-        console.log("aggregator_cron child process exited with code", code);
-    });
+  blockFetcher.on('close', function (code) {
+    console.log("aggregator_cron child process exited with code", code);
+  });
 }
 
 
 function writeLogsToFile(fileName, data) {
-    const LOGS_DIR = "./log/";
-    var stream = fs.createWriteStream(LOGS_DIR + fileName, {flags:'a'});
-    stream.write(data + "\n");
-    stream.end();
+  const LOGS_DIR = "./log/";
+  var stream = fs.createWriteStream(LOGS_DIR + fileName, {flags: 'a'});
+  stream.write(data + "\n");
+  stream.end();
 }
