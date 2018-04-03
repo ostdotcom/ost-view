cde1a17248d121cd2fd10c577bb89d2d058ed8cb
amanbarbaria <aman@pepo.com>
2018-04-03 16:36:45 +0530

    migration for cron details table

--- migrations/20180403104339-create-cron-details.js
+++ migrations/20180403104339-create-cron-details.js
@@ -5,9 +5,9 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
@@ -15,13 +15,31 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return null;
+  return createCronDetailsTable(db)
+    .then(function () {
+      return createIndexOnCronDetailsTable(db);
+    });
+
+  // add initial row for addressProcess Cron
 };
 
 exports.down = function(db) {
   return null;
 };
 
-exports._meta = {
-  "version": 1
+//cron_details
+//id, cron_name, data, tokens, created_at, updated_at
+const createCronDetailsTable = function (db) {
+  return db.createTable('cron_details', {
+    id: {type: 'bigint', notNull: true, primaryKey: true, autoIncrement: true},
+    cron_name: {type: 'string', notNull: true},
+    data: {type: 'string'},
+    created_at:{type: 'datetime', notNull: true},
+    updated_at:{type: 'datetime', notNull: true}
+  });
+};
+
+const createIndexOnCronDetailsTable = function (db) {
+  db.addIndex('cron_details', 'cd_cn_index', 'cron_name', true);
+
 };
