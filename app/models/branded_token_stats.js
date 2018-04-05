7ce4f747668dd773e91573999f44235d45c95d74
pankajkeswani <pankaj@pepo.com>
2018-04-05 15:38:43 +0530

    Branded Token Stats model

---
+++ app/models/branded_token_stats.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const rootPrefix = '../..'
+  , coreConstants = require(rootPrefix + '/config/core_constants')
+  , ModelBaseKlass = require(rootPrefix + '/app/models/base')
+;
+
+
+const BrandedTokenStatsKlass = function (chainId) {
+  const oThis = this
+  ;
+
+  oThis.chainId = chainId;
+  ModelBaseKlass.call(oThis, {chainId: chainId});
+};
+
+BrandedTokenStatsKlass.prototype = Object.create(ModelBaseKlass.prototype);
+
+/*
+ * Public methods
+ */
+const BrandedTokenStatsPrototype = {
+
+  tableName: coreConstants.BRANDED_TOKENS_STATS_TABLE_NAME,
+};
+
+Object.assign(BrandedTokenStatsKlass.prototype, BrandedTokenStatsPrototype);
+
+BrandedTokenStatsKlass.DATA_SEQUENCE_ARRAY = ['contract_address_id', 'token_holders', 'token_transfers', 'total_supply', 'token_ost_volume', 'market_cap', 'circulation'];
+
+module.exports = BrandedTokenStatsKlass;
+
+
+// ttk = require('./app/models/branded_token_stats')
+// new ttk().select('*').where('id>1').limit('10').fire().then(console.log);
--- migrations/20180405080648-create-branded-token-stats.js
+++ migrations/20180405080648-create-branded-token-stats.js
@@ -32,6 +32,12 @@ const createBrandedTokenStatsTable = function (db) {
   return db.createTable(constants.BRANDED_TOKENS_STATS_TABLE_NAME, {
     id: {type: 'bigint', notNull: true, primaryKey: true, autoIncrement: true},
     contract_address_id: {type: 'bigint', notNull: true},
+    token_holders: {type: 'int', notNull: true, default: 0},
+    token_transfers: {type: 'int', notNull: true, default: 0},
+    total_supply: {type:'decimal', length:'40,0',notNull: false},
+    token_ost_volume: {type:'decimal', length:'40,0',notNull: false},
+    market_cap: {type:'decimal', length:'40,0',notNull: false},
+    circulation: {type:'decimal', length:'40,0',notNull: false},
     created_at:{type: 'datetime', notNull: true},
     updated_at:{type: 'datetime', notNull: true}
   });
