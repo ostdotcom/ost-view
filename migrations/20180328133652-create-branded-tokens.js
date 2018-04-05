c26edbb1cbba6f2cd4769c514e58bee0ea1948cd
Sachin <sachin@pepo.com>
2018-04-05 17:20:43 +0530

    migration change

--- migrations/20180328133652-create-branded-tokens.js
+++ migrations/20180328133652-create-branded-tokens.js
@@ -51,7 +51,7 @@ const deleteBrandedTokenTable = function (db) {
 
 const createInitialRowsForBrandedTokenTable = function (db) {
   const currentDate = new Date();
-  const timestamp = currentDate.getTime();
-  const sqlStatement = "INSERT INTO " + constants.BRANDED_TOKENS_TABLE_NAME + " VALUES (0, 'OST', 0, 'OST', '0', 1, '',"+ timestamp +",'" + timestamp + "', '"+ timestamp +"')";
+  const timestamp = Number(currentDate.getTime()/1000);
+  const sqlStatement = "INSERT INTO " + constants.BRANDED_TOKENS_TABLE_NAME + " VALUES (0, 'OST', 0, 'OST', '0', 1, '',"+ timestamp +",'" + currentDate + "', '"+ currentDate +"')";
   return db.runSql(sqlStatement);
 };
