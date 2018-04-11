e1ff99278a3000607ceef5804ba570c4f36b8b3a
Sachin Chauhan <sachin@pepo.com>
2018-04-11 16:21:45 +0530

    Change in fetch graph services

--- app/services/token_details/get_transaction_by_type.js
+++ app/services/token_details/get_transaction_by_type.js
@@ -3,10 +3,11 @@
 const rootPrefix = "../../.."
   , responseHelper = require(rootPrefix + '/lib/formatter/response')
   , coreConstant = require(rootPrefix + '/config/core_constants')
+  , AddressesIdMapCacheKlass = require(rootPrefix + '/lib/cache_multi_management/addressIdMap.js')
 ;
 
 /**
- * Get details of transaction by type for a branded token
+ * Get transaction type for a branded token
  *
  * @param {object} params - this is object with keys.
  *                 chainId - Chain Id.
@@ -14,25 +15,36 @@ const rootPrefix = "../../.."
  *
  * @Constructor
  */
-const GetTransactionByTypeGraphKlass = function(params){
+const GeTransactionTypeGraphKlass = function(params){
   const oThis = this;
 
   oThis.chainId = params.chainId;
-  oThis.contractAddress = params.contractAddress
+  oThis.contractAddress = params.contractAddress;
+  oThis.duration = params.duration;
 };
 
 
-GetTransactionByTypeGraphKlass.prototype = {
+GeTransactionTypeGraphKlass.prototype = {
 
   /**
-   * Perform operation of getting transaction by type graph details
+   * Perform operation of getting recent token transfers details
    *
    * @return {Promise<void>}
    */
   perform: async function () {
+    const oThis = this;
 
+    const response  = await new AddressesIdMapCacheKlass({chain_id: oThis.chainId, addresses:[oThis.contractAddress]}).fetch()
+      , responseData = response.data;
+    if (response.isFailure() || !responseData[oThis.contractAddress]){
+      throw "GeTransactionTypeGraphKlass :: AddressesIdMapCacheKlass :: response Failure Or contract Address not found ::" + oThis.contractAddress;
+    }
+
+    const contractAddressId = responseData[oThis.contractAddress];
+
+    //TODO: fetch graph data
     return Promise.resolve(responseHelper.successWithData());
   }
-}
+};
 
-module.exports = GetTransactionByTypeGraphKlass;
+module.exports = GeTransactionTypeGraphKlass;
